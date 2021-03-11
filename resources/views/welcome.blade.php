674a28f0f6fcbb5bb496327ec17b8d4d809ce6cf
ayesaapa272 <MY_NAME@example.com>
2021-03-11 15:23:29 +0600

    change color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,3 @@
 <h1>Hello Ayesa</h1>
-<p style="color:red;">The App Directory
+<p style="color:green;">The App Directory
 The app directory contains the core code of your application. We'll explore this directory in more detail soon; however, almost all of the classes in your application will be in this directory.</p>
